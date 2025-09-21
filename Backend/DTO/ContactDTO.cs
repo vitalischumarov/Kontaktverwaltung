65e2a1aa2465e70f07346b48c2624280f93a04e9
Vitali <vitali@Vitalis-Mac-mini.fritz.box>
2025-09-21 21:25:13 +0200

    DTO created

---
+++ Backend/DTO/ContactDTO.cs
@@ -0,0 +1,13 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Backend.DTO;
+
+public class ContactDTO
+{
+    private string firstName { get; set; }
+    private string lastName { get; set; }
+    private string email { get; set; }
+    private string phone { get; set; }
+    private string address { get; set; }
+    private string birthDate { get; set; }
+}
