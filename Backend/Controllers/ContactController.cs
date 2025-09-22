7a4c88cbf6e17c9116dc7507e42c390ac085f311
Vitali <vitali@Vitalis-Mac-mini.fritz.box>
2025-09-22 16:43:42 +0200

    GET-Request Demo done

--- Backend/Controllers/ContactController.cs
+++ Backend/Controllers/ContactController.cs
@@ -3,13 +3,24 @@ using Backend.DTO;
 
 namespace Backend.Controllers;
 
+[Route("contact")]
+[ApiController]
 public class ContactController : ControllerBase
 {
+    private List<string> namen = new List<string>()
+    {
+        "Vitali","Tasi","Coralie","Yumi"
+    };
+
     //GET
-    
+    [HttpGet]
+    public async Task<List<string>> getData()
+    {
+        return namen;
+    }
     //POST
-    
+
     //PUT
-    
+
     //DELETE
 }
--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e2a1aa2465e70f07346b48c2624280f93a04e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e54d77496efe76bf21dc12d39362acbeede3d70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
